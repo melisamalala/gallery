1f597fee1c7af4e8d5a286756bbfa65bf95f8bd7
melisamalala <melissamalala@gmail.com>
2018-09-25 15:49:17 +0300

    Added test for location and it works

--- newsimage/tests.py
+++ newsimage/tests.py
@@ -1,3 +1,20 @@
 from django.test import TestCase
 
 # Create your tests here.
+from .models import Location, tags
+
+class LocationTestClass(TestCase):
+
+    #Set up method the test for location and instantiating the location object
+
+
+    def setUp(self):
+        self.nairobi = Location(name = 'Nairobi')
+
+    #Testing instance
+
+    def test_instance(self):
+
+        self.assertTrue(isinstance(self.nairobi, Location))
+
+

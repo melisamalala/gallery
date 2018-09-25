751b687dce2536b6ea5570b6ef0a85b4e3d7139b
melisamalala <melissamalala@gmail.com>
2018-09-25 15:39:17 +0300

    delete sqlite3 file

--- newsimage/models.py
+++ newsimage/models.py
@@ -1,3 +1,31 @@
 from django.db import models
 
 # Create your models here.
+import datetime as dt
+
+class Location(models.Model):
+    name = models.CharField(max_length=30)
+
+    def __str__(self):
+        return self.name
+
+    def save_location(self):
+        self.save()
+
+    def delete_location(self):
+        self.delete()
+
+
+class tags(models.Model):
+    name = models.CharField(max_length=30)
+
+    def __str__(self):
+        return self.name
+
+    def save_tags(self):
+        self.save()
+
+    def delete_tags(self):
+        self.delete()
+
+
